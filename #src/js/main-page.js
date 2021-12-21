03b4b789ed4bc43ea1d08737d862e31a00c3548a
Anton <kamarhin_anton@ukr.net>
2021-12-21 18:18:14 +0200

    feat: add logos block

---
+++ #src/js/main-page.js
@@ -0,0 +1,31 @@
+window.addEventListener('DOMContentLoaded', function () {
+    sliderLogo(),
+        sliderLogoCenter()
+})
+
+const sliderLogo = () => {
+    new Swiper('.slider-logo', {
+        grabCursor: true,
+        slidesPerView: 2,
+        autoplay: {
+            delay: 0,
+        },
+        initialSlide: 1,
+        spaceBetween: 16,
+        speed: 3000,
+        loop: true,
+    });
+}
+const sliderLogoCenter = () => {
+    new Swiper('.slider-logo-center', {
+        grabCursor: true,
+        slidesPerView: 1,
+        autoplay: {
+            delay: 0,
+        },
+        initialSlide: 1,
+        spaceBetween: 16,
+        speed: 3000,
+        loop: true,
+    });
+}
