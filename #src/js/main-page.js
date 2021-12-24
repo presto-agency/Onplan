eba58a69abb4365d36f65f5f111f0eb36761776e
Anton <kamarhin_anton@ukr.net>
2021-12-24 11:10:10 +0200

    feat: add block tabs

--- #src/js/main-page.js
+++ #src/js/main-page.js
@@ -1,31 +1,27 @@
 window.addEventListener('DOMContentLoaded', function () {
-    // sliderLogo(),
-    // sliderLogoCenter()
+    callTabs()
 })
 
-const sliderLogo = () => {
-    new Swiper('.slider-logo', {
-        grabCursor: true,
-        slidesPerView: 2,
-        // autoplay: {
-        //     delay: 0,
-        // },
-        initialSlide: 1,
-        spaceBetween: 16,
-        speed: 3000,
-        loop: true,
-    });
-}
-const sliderLogoCenter = () => {
-    new Swiper('.slider-logo-center', {
-        grabCursor: true,
-        slidesPerView: 1,
-        // autoplay: {
-        //     delay: 0,
-        // },
-        initialSlide: 1,
-        spaceBetween: 16,
-        speed: 3000,
-        loop: true,
-    });
+const callTabs = () => {
+    let elementsLink = document.querySelectorAll('.trigger-obj');
+    let tabsBtns = document.querySelectorAll('.tabs__item');
+    const tabs = document.querySelector('.tabs__body');
+    const menu = document.querySelector('.tabs__items');
+    elementsLink.forEach(link => link.addEventListener('click', function () {
+        let attr = this.getAttribute('data-attr');
+        const activeBtn = tabs.querySelector('.active');
+        let subWindow = document.getElementById(attr);
+        activeBtn.classList.remove('active');
+        subWindow.classList.add('active');
+    }))
+    tabsBtns.forEach(btn => btn.addEventListener('click', function () {
+        const activeBtn = menu.querySelector('.active');
+        if (!!activeBtn) {
+            activeBtn.classList.remove('active')
+            btn.classList.add('active')
+        }
+        else {
+            btn.classList.add('active')
+        }
+    }))
 }
