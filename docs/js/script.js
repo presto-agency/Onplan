398298c840b10c6c40e587ee3084a878b8050e37
Anton <kamarhin_anton@ukr.net>
2021-12-30 12:07:45 +0200

    fix: hot fixes-31

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,41 +1,44 @@
 window.addEventListener('DOMContentLoaded', function () {
+    const headerMenu = document.querySelector('.header');
+if (!!headerMenu) {
     (function burgerAnimation() {
-    const burger = document.querySelector('.header__burger');
-    const menu = document.querySelector('.header');
-    const body = document.querySelector('body');
-    const mobBtns = document.querySelector('.mobile-menu-btns');
-    burger.onclick = () => {
-        addClass(burger, 'active')
-        addClass(menu, 'active')
-        addClass(body, 'body-lock')
-        addClass(mobBtns, 'active')
-    }
-    function addClass(obj, classname) {
-        obj.classList.toggle(classname)
-    }
-}());
-
-(function callSub() {
-    let elementsLink = document.querySelectorAll('.trigger');
-    elementsLink.forEach(link => link.addEventListener('click', function () {
-        let attr = this.getAttribute('data-attr');
+        const burger = document.querySelector('.header__burger');
         const menu = document.querySelector('.header');
-        let subWindow = document.getElementById(attr);
-        subWindow.classList.add('active');
-        menu.classList.add('active-sub');
-    }))
-}());
+        const body = document.querySelector('body');
+        const mobBtns = document.querySelector('.mobile-menu-btns');
+        burger.onclick = () => {
+            addClass(burger, 'active')
+            addClass(menu, 'active')
+            addClass(body, 'body-lock')
+            addClass(mobBtns, 'active')
+        }
+        function addClass(obj, classname) {
+            obj.classList.toggle(classname)
+        }
+    }());
 
-(function closeSub() {
-    let subClose = document.querySelectorAll('.sub-close');
-    subClose.forEach(close => close.addEventListener('click', function () {
-        let attr = this.getAttribute('data-close');
-        const menu = document.querySelector('.header');
-        let subMenu = document.getElementById(attr);
-        subMenu.classList.remove('active');
-        menu.classList.remove('active-sub');
-    }))
-})();
+    (function callSub() {
+        let elementsLink = document.querySelectorAll('.trigger');
+        elementsLink.forEach(link => link.addEventListener('click', function () {
+            let attr = this.getAttribute('data-attr');
+            const menu = document.querySelector('.header');
+            let subWindow = document.getElementById(attr);
+            subWindow.classList.add('active');
+            menu.classList.add('active-sub');
+        }))
+    }());
+
+    (function closeSub() {
+        let subClose = document.querySelectorAll('.sub-close');
+        subClose.forEach(close => close.addEventListener('click', function () {
+            let attr = this.getAttribute('data-close');
+            const menu = document.querySelector('.header');
+            let subMenu = document.getElementById(attr);
+            subMenu.classList.remove('active');
+            menu.classList.remove('active-sub');
+        }))
+    })();
+}
 
 (function swiperComments() {
     new Swiper('.swiper-integration__swiper', {
