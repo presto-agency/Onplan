ea7ecce43bef1c48ff494fdc2184df6a95e31e01
Anton <kamarhin_anton@ukr.net>
2022-01-03 15:22:13 +0200

    feat: add js function lazy-loading

--- #src/js/common.js
+++ #src/js/common.js
@@ -70,3 +70,20 @@ if (!!headerMenu) {
         },
     });
 }());
+
+(function lazyLoad() {
+    const images = document.querySelectorAll('img[data-src]');
+    const sources = document.querySelectorAll('source[data-srcset]');
+
+    for (let i = 0; i < images.length; i++) {
+        let dataSrc = images[i].getAttribute('data-src');
+        images[i].setAttribute('src', dataSrc);
+        images[i].removeAttribute('data-src');
+    }
+
+    for (let i = 0; i < sources.length; i++) {
+        let dataSrcSet = sources[i].getAttribute('data-srcset');
+        sources[i].setAttribute('srcset', dataSrcSet);
+        sources[i].removeAttribute('data-srcset');
+    }
+}());
