45b1000da6266598f6188802972e0bf12f2a5a65
Anton <kamarhin_anton@ukr.net>
2022-01-17 18:29:49 +0200

    feat: finishing pricing

--- #src/js/pricing.js
+++ #src/js/pricing.js
@@ -4,34 +4,64 @@ if (!!pricing) {
         let isScrolling = false;
         const menu = document.querySelector('.pricing__relative-title');
         const btns = document.querySelector('.table-price');
-        if (document.documentElement.clientWidth > 1024) {
-            window.addEventListener("scroll", throttleScroll, false);
-            function throttleScroll(e) {
-                if (isScrolling == false) {
-                    window.requestAnimationFrame(function () {
-                        scrolling(e);
-                        isScrolling = false;
-                    });
-                }
-                isScrolling = true;
+        window.addEventListener("scroll", throttleScroll, false);
+        function throttleScroll(e) {
+            if (isScrolling == false) {
+                window.requestAnimationFrame(function () {
+                    scrolling(e);
+                    isScrolling = false;
+                });
             }
-
-            function isFullyVisible(el) {
-                let elementBoundary = el.getBoundingClientRect();
-                let top = elementBoundary.top;
-                let bottom = elementBoundary.bottom;
-                return ((top <= 0) && (bottom >= 0));
-            }
-
-            function scrolling() {
-                if (isFullyVisible(btns)) {
-                    menu.classList.add('active');
-                }
-                else {
-                    menu.classList.remove('active');
-                }
-            }
-            scrolling();
+            isScrolling = true;
         }
+
+        function isFullyVisible(el) {
+            let elementBoundary = el.getBoundingClientRect();
+            let top = elementBoundary.top;
+            let bottom = elementBoundary.bottom;
+            return ((top <= 0) && (bottom >= 0));
+        }
+
+        function scrolling() {
+            if (isFullyVisible(btns)) {
+                menu.classList.add('active');
+            }
+            else {
+                menu.classList.remove('active');
+            }
+        }
+        scrolling();
+    }());
+    (function scrollName() {
+        let isScrolling = false;
+        window.addEventListener("scroll", throttleScroll, false);
+        function throttleScroll(e) {
+            if (isScrolling == false) {
+                window.requestAnimationFrame(function () {
+                    scrolling(e);
+                    isScrolling = false;
+                });
+            }
+            isScrolling = true;
+        }
+
+        const elementsPage = document.querySelectorAll('.table-price__bottom-table');
+        function isFullyVisible(el) {
+            let topOfElements = el.getBoundingClientRect().top;
+            let bottomOfElements = el.getBoundingClientRect().bottom;
+            let topValue = + topOfElements;
+            let bottomValue = + bottomOfElements;
+            return (((topValue <= 130) && (topValue > 0)) || ((bottomValue <= 130) && (bottomValue > 0)));
+        }
+        function scrolling() {
+            elementsPage.forEach(el => {
+                if (isFullyVisible(el)) {
+                    let idEl = el.id;
+                    const changeBtn = document.getElementById('change-btn');
+                    changeBtn.innerHTML = idEl;
+                }
+            });
+        }
+        scrolling();
     }());
 }
