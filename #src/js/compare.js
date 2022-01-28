49925511d85f4fae7f5bcd377ac3709aebe36fee
Anton <kamarhin_anton@ukr.net>
2022-01-28 17:56:30 +0200

    feat: add js to relative block

--- #src/js/compare.js
+++ #src/js/compare.js
@@ -4,6 +4,12 @@
     changeMargin(scrollObjs, scrollTablewidth)
 }());
 
+(function marginRelativeTable() {
+    let scrollTablewidth = document.querySelector('.relative-title__relative-list').clientWidth;
+    let scrollObjs = document.querySelectorAll('.relative-title__link');
+    changeMargin(scrollObjs, scrollTablewidth)
+}());
+
 (function marginBottomTables() {
     let bottomTables = document.querySelectorAll('.bottom-scrolling-table');
     bottomTables.forEach(table => {
@@ -14,7 +20,7 @@
 }());
 
 (function scrollingAll() {
-    let scrollers = document.querySelectorAll('.bottom-scrolling-table, .table-scroll');
+    let scrollers = document.querySelectorAll('.bottom-scrolling-table, .table-scroll, .relative-title__relative-list');
 
     scrollers.forEach(element => {
         element.addEventListener('scroll', function (e) {
@@ -31,7 +37,7 @@
 
 (function grabCursorTop() {
     if (window.screen.width > 1024) {
-        let containers = document.querySelectorAll('.bottom-scrolling-table, .table-scroll');
+        let containers = document.querySelectorAll('.bottom-scrolling-table, .table-scroll, .relative-title__relative-list');
         let startX;
         let scrollLeft;
         let isDown;
@@ -64,11 +70,64 @@
     }
 }());
 
+(function compareRelativeBlock() {
+    let isScrolling = false;
+    const menu = document.querySelector('.relative-title');
+    const btns = document.querySelector('.table-price');
+    window.addEventListener("scroll", throttleScroll, false);
+    function throttleScroll(e) {
+        if (isScrolling == false) {
+            window.requestAnimationFrame(function () {
+                scrolling(e);
+                isScrolling = false;
+            });
+        }
+        isScrolling = true;
+    }
+
+    function isFullyVisible(el) {
+        let elementBoundary = el.getBoundingClientRect();
+        let top = elementBoundary.top;
+        let bottom = elementBoundary.bottom;
+        return ((top <= 0) && (bottom >= 0));
+    }
+
+    function scrolling() {
+        if (isFullyVisible(btns)) {
+            menu.classList.add('active');
+        }
+        else {
+            menu.classList.remove('active');
+        }
+    }
+    scrolling();
+}());
+
+// (function scrollName() {
+//     window.addEventListener("scroll", scrolling, true);
+//     function scrolling() {
+//         const elementsPage = document.querySelectorAll('.table-price__bottom-table');
+//         elementsPage.forEach(el => {
+//             if (isFullyVisible(el)) {
+//                 let idEl = el.id;
+//                 const changeBtn = document.getElementById('change-btn');
+//                 changeBtn.innerHTML = idEl;
+//             }
+//         });
+//     }
+//     function isFullyVisible(el) {
+//         let topOfElements = el.getBoundingClientRect().top;
+//         let bottomOfElements = el.getBoundingClientRect().bottom;
+//         return (((topOfElements <= 130) && (bottomOfElements > 0)));
+//     }
+// }());
+
 function changeMargin(elements, tableWidth) {
+    let widthOfLogo = elements[0].clientWidth;
     let array = [];
     elements.forEach(el => {
         array.push(el)
     })
     let lastElement = array[array.length - 1];
-    lastElement.style.margin = `0 ${tableWidth - 191}px 0 0`;
+    lastElement.style.margin = `0 ${tableWidth - widthOfLogo}px 0 0`;
 }
--- docs/js/script.js
+++ docs/js/script.js
@@ -465,6 +465,12 @@ if (!!about) {
     changeMargin(scrollObjs, scrollTablewidth)
 }());
 
+(function marginRelativeTable() {
+    let scrollTablewidth = document.querySelector('.relative-title__relative-list').clientWidth;
+    let scrollObjs = document.querySelectorAll('.relative-title__link');
+    changeMargin(scrollObjs, scrollTablewidth)
+}());
+
 (function marginBottomTables() {
     let bottomTables = document.querySelectorAll('.bottom-scrolling-table');
     bottomTables.forEach(table => {
@@ -475,7 +481,7 @@ if (!!about) {
 }());
 
 (function scrollingAll() {
-    let scrollers = document.querySelectorAll('.bottom-scrolling-table, .table-scroll');
+    let scrollers = document.querySelectorAll('.bottom-scrolling-table, .table-scroll, .relative-title__relative-list');
 
     scrollers.forEach(element => {
         element.addEventListener('scroll', function (e) {
@@ -492,7 +498,7 @@ if (!!about) {
 
 (function grabCursorTop() {
     if (window.screen.width > 1024) {
-        let containers = document.querySelectorAll('.bottom-scrolling-table, .table-scroll');
+        let containers = document.querySelectorAll('.bottom-scrolling-table, .table-scroll, .relative-title__relative-list');
         let startX;
         let scrollLeft;
         let isDown;
@@ -525,13 +531,66 @@ if (!!about) {
     }
 }());
 
+(function compareRelativeBlock() {
+    let isScrolling = false;
+    const menu = document.querySelector('.relative-title');
+    const btns = document.querySelector('.table-price');
+    window.addEventListener("scroll", throttleScroll, false);
+    function throttleScroll(e) {
+        if (isScrolling == false) {
+            window.requestAnimationFrame(function () {
+                scrolling(e);
+                isScrolling = false;
+            });
+        }
+        isScrolling = true;
+    }
+
+    function isFullyVisible(el) {
+        let elementBoundary = el.getBoundingClientRect();
+        let top = elementBoundary.top;
+        let bottom = elementBoundary.bottom;
+        return ((top <= 0) && (bottom >= 0));
+    }
+
+    function scrolling() {
+        if (isFullyVisible(btns)) {
+            menu.classList.add('active');
+        }
+        else {
+            menu.classList.remove('active');
+        }
+    }
+    scrolling();
+}());
+
+// (function scrollName() {
+//     window.addEventListener("scroll", scrolling, true);
+//     function scrolling() {
+//         const elementsPage = document.querySelectorAll('.table-price__bottom-table');
+//         elementsPage.forEach(el => {
+//             if (isFullyVisible(el)) {
+//                 let idEl = el.id;
+//                 const changeBtn = document.getElementById('change-btn');
+//                 changeBtn.innerHTML = idEl;
+//             }
+//         });
+//     }
+//     function isFullyVisible(el) {
+//         let topOfElements = el.getBoundingClientRect().top;
+//         let bottomOfElements = el.getBoundingClientRect().bottom;
+//         return (((topOfElements <= 130) && (bottomOfElements > 0)));
+//     }
+// }());
+
 function changeMargin(elements, tableWidth) {
+    let widthOfLogo = elements[0].clientWidth;
     let array = [];
     elements.forEach(el => {
         array.push(el)
     })
     let lastElement = array[array.length - 1];
-    lastElement.style.margin = `0 ${tableWidth - 191}px 0 0`;
+    lastElement.style.margin = `0 ${tableWidth - widthOfLogo}px 0 0`;
 }
 })
 
