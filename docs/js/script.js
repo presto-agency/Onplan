4c640023e060f4366527f74724ea0d511a1fea3b
Anton <kamarhin_anton@ukr.net>
2022-02-02 16:35:56 +0200

    feat: finishing feature-template

--- docs/js/script.js
+++ docs/js/script.js
@@ -616,70 +616,81 @@ if (featureTemplate) {
         })
     }());
 
-    (function featureRelativeBlock() {
-        let isScrolling = false;
-        const menu = document.querySelector('.right-relative-block');
-        const btns = document.querySelector('.editor');
-        window.addEventListener("scroll", throttleScroll, false);
-        function throttleScroll(e) {
-            if (isScrolling == false) {
-                window.requestAnimationFrame(function () {
-                    scrolling(e);
-                    isScrolling = false;
-                });
-            }
-            isScrolling = true;
-        }
-
-        function isFullyVisible(el) {
-            let elementBoundary = el.getBoundingClientRect();
-            let top = elementBoundary.top;
-            let bottom = elementBoundary.bottom;
-            return ((top <= 182) && (bottom >= 0));
-        }
-
-        function scrolling() {
-            if (isFullyVisible(btns)) {
-                menu.classList.add('active-menu');
-            }
-            else {
-                menu.classList.remove('active-menu');
-            }
-        }
-        scrolling();
-    }());
-
-    (function feauterScrollColor() {
-        let isScrolling = false;
-        window.addEventListener("scroll", throttleScroll, false);
-        function throttleScroll(e) {
-            if (isScrolling == false) {
-                window.requestAnimationFrame(function () {
-                    scrolling(e);
-                    isScrolling = false;
-                });
-            }
-            isScrolling = true;
-        }
-
-        let elementsPage = document.querySelectorAll('.editor__content>h2');
-        function isFullyVisible(el) {
-            let topOfElements = el.getBoundingClientRect().top;
-            let bottomOfElements = el.getBoundingClientRect().bottom;
-            return (((topOfElements <= 200) && (bottomOfElements > 0)));
-        }
-        function scrolling() {
-            elementsPage.forEach(el => {
-                if (isFullyVisible(el)) {
-                    let idEl = el.id;
-                    const activeBtns = featureTemplate.querySelectorAll('.active-color');
-                    let btns = document.querySelectorAll(`a[href='#${idEl}']`);
-                    changeClass(activeBtns, btns)
+    if (window.screen.width > 1023) {
+        (function featureRelativeBlock() {
+            let isScrolling = false;
+            const menu = document.querySelector('.right-relative-block');
+            const btns = document.querySelector('.editor');
+            window.addEventListener("scroll", throttleScroll, false);
+            function throttleScroll(e) {
+                if (isScrolling == false) {
+                    window.requestAnimationFrame(function () {
+                        scrolling(e);
+                        isScrolling = false;
+                    });
                 }
+                isScrolling = true;
+            }
+
+            function isFullyVisible(el) {
+                let elementBoundary = el.getBoundingClientRect();
+                let top = elementBoundary.top;
+                let bottom = elementBoundary.bottom;
+                return ((top <= 182) && (bottom >= 0));
+            }
+
+            function scrolling() {
+                if (isFullyVisible(btns)) {
+                    menu.classList.add('active-menu');
+                }
+                else {
+                    menu.classList.remove('active-menu');
+                }
+            }
+            scrolling();
+        }());
+
+        (function feauterScrollColor() {
+            let isScrolling = false;
+            window.addEventListener("scroll", throttleScroll, false);
+            function throttleScroll(e) {
+                if (isScrolling == false) {
+                    window.requestAnimationFrame(function () {
+                        scrolling(e);
+                        isScrolling = false;
+                    });
+                }
+                isScrolling = true;
+            }
+
+            let elementsPage = document.querySelectorAll('.editor__content>h2');
+            function isFullyVisible(el) {
+                let topOfElements = el.getBoundingClientRect().top;
+                let bottomOfElements = el.getBoundingClientRect().bottom;
+                return (((topOfElements <= 200) && (bottomOfElements > 0)));
+            }
+            function scrolling() {
+                elementsPage.forEach(el => {
+                    if (isFullyVisible(el)) {
+                        let idEl = el.id;
+                        const activeBtns = featureTemplate.querySelectorAll('.active-color');
+                        let btns = document.querySelectorAll(`a[href='#${idEl}']`);
+                        changeClass(activeBtns, btns)
+                    }
+                });
+            }
+            scrolling();
+        }());
+
+        const changeClass = (activeBtns, els) => {
+            activeBtns.forEach(aBtn => {
+                aBtn.classList.remove('active-color')
+            });
+            els.forEach(el => {
+                el.classList.add('active-color')
             });
         }
-        scrolling();
-    }());
+    }
 
     (function anchorAnimationFeature() {
         const anchors = document.querySelectorAll('a[href*="#"]');
@@ -694,15 +705,6 @@ if (featureTemplate) {
             })
         }
     }());
-
-    const changeClass = (activeBtns, els) => {
-        activeBtns.forEach(aBtn => {
-            aBtn.classList.remove('active-color')
-        });
-        els.forEach(el => {
-            el.classList.add('active-color')
-        });
-    }
 }
 })
 
