07003ea85910a7ebd3cb5c2bce7bfd44e20f7c17
Anton <kamarhin_anton@ukr.net>
2022-02-04 12:57:56 +0200

    feat: finishing mobile for filter

---
+++ #src/js/knowledge-hub.js
@@ -0,0 +1,129 @@
+const knowledgeHub = document.querySelector('.knowledge-hub');
+if (knowledgeHub) {
+    (function swiperTemplate() {
+        new Swiper('.swiper-template', {
+            navigation: {
+                nextEl: '.load-more-template',
+            },
+            grabCursor: true,
+            creativeEffect: {
+                prev: {
+                    translate: ["-120%", 0, -1],
+                },
+                next: {
+                    translate: ["120%", 0, 0],
+                },
+            },
+            speed: 500,
+            spaceBetween: 32,
+            preloadImages: false,
+            lazy: true,
+            breakpoints: {
+                768: {
+                    slidesPerView: 2,
+                    slidesPerGroup: 2,
+                    direction: "horizontal",
+                },
+                0: {
+                    slidesPerView: 2,
+                    slidesPerGroup: 2,
+                    direction: "vertical",
+                    touchRatio: 0,
+                }
+            }
+        });
+    }());
+    (function swiperGuide() {
+        new Swiper('.swiper-guide', {
+            navigation: {
+                nextEl: '.load-more-guide',
+            },
+            grabCursor: true,
+            creativeEffect: {
+                prev: {
+                    translate: ["-120%", 0, -1],
+                },
+                next: {
+                    translate: ["120%", 0, 0],
+                },
+            },
+            speed: 500,
+            spaceBetween: 32,
+            preloadImages: false,
+            lazy: true,
+            breakpoints: {
+                768: {
+                    slidesPerView: 2,
+                    slidesPerGroup: 2,
+                    direction: "horizontal",
+                },
+                0: {
+                    slidesPerView: 2,
+                    slidesPerGroup: 2,
+                    direction: "vertical",
+                    touchRatio: 0,
+                }
+            }
+        });
+    }());
+    (function sortSelect() {
+        let blockSort = document.getElementById("sort");
+        if (blockSort) {
+            let blockList = document.getElementById("sort-list");
+            let elemItemSelect = document.querySelectorAll('.sort-select__item');
+            let elemCurrent = document.getElementById('current-value');
+            let active = document.querySelector('#sort-list li.active');
+            let current = document.querySelector('#current-value');
+            current.textContent = active.textContent;
+
+            const hideSelectMenu = () => {
+                blockList.classList.remove('container--active');
+                blockSort.classList.remove('turn-arrow');
+                blockList.style.maxHeight = 0;
+            };
+
+            const showSelectMenu = () => {
+                blockList.classList.add('container--active');
+                blockList.style.maxHeight = blockList.scrollHeight + 'px';
+                blockSort.classList.add('turn-arrow');
+            };
+
+            const onMouseScroll = () => {
+                if (!blockList.classList.contains('container--active')) {
+                    return;
+                };
+                if (window.pageYOffset > 0) {
+                    hideSelectMenu();
+                }
+            };
+
+            const onOutsideClick = (e) => {
+                if (!blockList.classList.contains('container--active')) {
+                    return;
+                };
+
+                if (!e.target.classList.contains('sort-select__item') && !e.target.classList.contains('sort-select__current')) {
+                    hideSelectMenu();
+                };
+            };
+
+            const onSelectClick = () => {
+                blockList.classList.contains('container--active')
+                    ? hideSelectMenu()
+                    : showSelectMenu();
+            };
+
+            blockSort.addEventListener('click', onSelectClick);
+            window.addEventListener('click', onOutsideClick);
+            window.addEventListener('scroll', onMouseScroll);
+
+            elemItemSelect.forEach(item => item.addEventListener('click', function () {
+                let elemItemSelectActive = document.querySelectorAll('.sort-select__item.active');
+                elemItemSelectActive.forEach(itemActive => itemActive.classList.remove('active'));
+                item.classList.add('active');
+                elemCurrent.textContent = item.textContent;
+                elemCurrent.setAttribute('data-value', item.getAttribute('data-value'));
+            }))
+        }
+    }());
+}
