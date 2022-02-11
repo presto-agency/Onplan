eeea3c68afa7301889f9378212ed86361b794b8b
Anton <kamarhin_anton@ukr.net>
2022-02-11 18:39:06 +0200

    feat: finishing app-listing

---
+++ #src/js/app-listing.js
@@ -0,0 +1,35 @@
+const appListing = document.querySelector('.app-listing');
+if (!!appListing) {
+    (function swiperApp() {
+        new Swiper('.slider-app__swiper', {
+            navigation: {
+                nextEl: '.slider-app__next',
+                prevEl: '.slider-app__prev'
+            },
+            pagination: {
+                el: ".slider-app__pagination",
+                clickable: true
+            },
+            slidesPerView: 1,
+            slidesPerGroup: 1,
+            grabCursor: true,
+            effect: "creative",
+            creativeEffect: {
+                prev: {
+                    translate: ["-120%", 0, -1],
+                },
+                next: {
+                    translate: ["120%", 0, 0],
+                },
+            },
+            speed: 500,
+            spaceBetween: 30,
+            preloadImages: true,
+            loop: true,
+            // lazy: {
+            //     loadOnTransitionStart: true,
+            //     loadPrevNext: true,
+            // },
+        });
+    }());
+}
