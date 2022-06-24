1e2330e1d5a20ffd94ed154b4cf20570f94c6849
Kamarhinanton <Kamarhin_anton@ukr.net>
2022-06-24 11:24:25 +0300

    feat: add custom preview

--- #src/js/app-listing.js
+++ #src/js/app-listing.js
@@ -4,4 +4,14 @@ if (!!appListing) {
         let containers = document.querySelectorAll('.card__element-wrapper');
         grabCursor(containers)
     }());
+    (function playVideo() {
+        let videos = document.querySelectorAll('.slider-app__video>.slider-app__preview');
+        if(videos.length>=1){
+            videos.forEach(video=>{
+                video.onclick = () =>{
+                    video.classList.add('active')
+                }
+            })
+        }
+    }());
 }
--- docs/js/script.js
+++ docs/js/script.js
@@ -763,6 +763,16 @@ if (!!appListing) {
         let containers = document.querySelectorAll('.card__element-wrapper');
         grabCursor(containers)
     }());
+    (function playVideo() {
+        let videos = document.querySelectorAll('.slider-app__video>.slider-app__preview');
+        if(videos.length>=1){
+            videos.forEach(video=>{
+                video.onclick = () =>{
+                    video.classList.add('active')
+                }
+            })
+        }
+    }());
 }
 
 const summaryPage = document.querySelector('.summary');
